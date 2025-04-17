4103e16873ade1e86ec74c0a74cff9b859059cc5
Celia Ho <celia.ho@gmail.com>
2025-04-16 20:15:19 -0400

    first commit

---
+++ FX1/FX1/src/App.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+        stage.setTitle("My First FX App");
+        stage.show();
+    }
+}
+
